c42d13be6ab2ff85ad6a92bff7e991fe0ed0c99f
Nastavshev Yuriy <yvnastavshev@sberbank.ru>
2024-05-15 23:52:26 +0300

    chore: start-lesson

--- src/App.js
+++ src/App.js
@@ -1,46 +1,10 @@
 // App.js
-import React, { useState } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Header from "./Header.jsx";
-import Sidebar from "./SideBar.js";
-import PostList from "./PostList.jsx";
-import NewPostForm from "./NewPostForm.js";
-import SearchBar from "./SearchBar.js";
-import Post from "./Post.js";
+import React from "react";
+
 
 function App() {
-  const [posts, setPosts] = useState([]);
-  const [pageCount, setPageCount] = useState(0);
-
-  const onSearch = () => {};
-
   return (
-    <Router>
-      <div>
-        <Header />
-        <div style={{ padding: "1rem" }}>
-          <Sidebar onSearch={onSearch} />
-
-          <SearchBar style={{ margin: "1rem" }} />
-          <Routes>
-            <Route
-              path="/"
-              element={
-                <PostList
-                  posts={posts}
-                  setPosts={setPosts}
-                  pageCount={pageCount}
-                  setPageCount={setPageCount}
-                />
-              }
-            />
-            <Route path="/new" element={<NewPostForm />} />
-            <Route path="/post/:id" element={<Post />} />
-            {/* Добавьте другие маршруты здесь */}
-          </Routes>
-        </div>
-      </div>
-    </Router>
+    <h1>AppComponent</h1>
   );
 }
 
